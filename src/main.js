eb90baa099a4c918a93fe010d30cb2bcc483d019
Allan Carvalho <allandcarv@gmail.com>
2019-07-18 14:24:25 -0300

    Administrative Pages

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import App from './App.vue'
 
 import './config/bootstrap'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO!!!!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZDNyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE1NjM0NTUwODQsImV4cCI6MTU2MzcxNDI4NH0.VHDHejJyudrwMaO4OvTCIe4qcgVC-HtOiFkViGTZkXM'
+
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
