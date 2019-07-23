01947c2005ddc8e8f44ce084dc0cbb519d582178
Allan Carvalho <allandcarv@gmail.com>
2019-07-23 15:06:17 -0300

    Responsive Menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
-    <Header title="Cod3r - Base de Conhecimento" 
-      :hideToggle="!user" 
-      :hideUserDropdown="!user"
-    />
+    <Header title="Cod3r - Base de Conhecimento" :hideToggle="!user" :hideUserDropdown="!user" />
     <Menu v-if="user" />
     <Loading v-if="validateToken" />
     <Content v-else />
     <Footer />
-
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import axios from 'axios'
+import { mapState } from "vuex";
+import axios from "axios";
 
-import { baseApiUrl, userKey } from '@/global'
+import { baseApiUrl, userKey } from "@/global";
 import Header from "./components/template/Header";
 import Menu from "./components/template/Menu";
 import Content from "./components/template/Content";
@@ -24,74 +20,78 @@ import Footer from "./components/template/Footer";
 import Loading from "./components/template/Loading";
 
 export default {
-  name: 'app',
+  name: "app",
   components: { Header, Menu, Content, Footer, Loading },
-  computed: mapState(['isMenuVisible', 'user']),
+  computed: mapState(["isMenuVisible", "user"]),
   data: function() {
     return {
       validatingToken: true
-    }
+    };
   },
   methods: {
     async validateToken() {
-      this.validateToken = true
+      this.validateToken = true;
 
-      const json = localStorage.getItem(userKey)
-      const userData = JSON.parse(json)
-      this.$store.commit('setUser', null)
+      const json = localStorage.getItem(userKey);
+      const userData = JSON.parse(json);
+      this.$store.commit("setUser", null);
 
       if (!userData) {
-        this.validateToken = false
-        return this.$router.push({ name: 'auth' })
+        this.validateToken = false;
+        return this.$router.push({ name: "auth" });
       }
 
-      const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+      const res = await axios.post(`${baseApiUrl}/validateToken`, userData);
 
       if (res.data) {
-        this.$store.commit('setUser', userData)
+        this.$store.commit("setUser", userData);
+
+        if (this.$mq === "xs" || this.$mq === "sm") {
+          this.$store.commit("toggleMenu", false);
+        }
       } else {
-        localStorage.removeItem(userKey)
-        this.$router.push({ name: 'auth' })
+        localStorage.removeItem(userKey);
+        this.$router.push({ name: "auth" });
       }
 
-      this.validatingToken = false
+      this.validatingToken = false;
     }
   },
   created() {
-    this.validateToken()
+    this.validateToken();
   }
-}
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Lato&display=swap");
 
-  * {
-    font-family: "Lato", sans-serif;
-  }
+* {
+  font-family: "Lato", sans-serif;
+}
 
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+}
 
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-    height: 100vh;
-    display: grid;
-    grid-template-rows: 60px 1fr 40px;
-    grid-template-columns: 300px 1fr;
-    grid-template-areas:
-      "header header"
-      "menu content"
-      "footer footer"
-  }
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "footer footer";
+}
 
-  #app.hide-menu {
-    grid-template-areas: 
-      "header header"
-      "content content"
-      "footer footer"
-  }
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
+}
 </style>
